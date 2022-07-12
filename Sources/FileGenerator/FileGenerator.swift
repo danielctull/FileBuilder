9cde165f5d30c0e6bf35ef738048077e47faa392
Daniel Tull <dt@danieltull.co.uk>
2022-07-12 18:07:27 +0100

    Add result builder for the file contents

--- Sources/FileGenerator/FileGenerator.swift
+++ Sources/FileGenerator/FileGenerator.swift
@@ -1,14 +1,38 @@
 
 public struct FileGenerator {
-
     public let indentation: Indentation
     public let lines: [Line]
-
+    
     public init(
         indentation: Indentation = .spaces(4),
-        lines: Line...
+        lines: [Line]
     ) {
         self.indentation = indentation
         self.lines = lines
+    }
+}
+
+// MARK: - Builder
+
+extension FileGenerator {
+
+    public init(indentation: Indentation, @Builder build: () -> [Line]) {
+        self.init(indentation: indentation, lines: build())
+    }
+
+    @resultBuilder
+    public enum Builder {
+
+        public static func buildPartialBlock(first: Line) -> [Line] {
+            [first]
+        }
+
+        public static func buildPartialBlock(accumulated: [Line], next: Line) -> [Line] {
+            accumulated + [next]
+        }
+
+        public static func buildFinalResult(_ component: [Line]) -> [Line] {
+            component
+        }
     }
 }
--- Tests/FileGeneratorTests/FileGeneratorTests.swift
+++ Tests/FileGeneratorTests/FileGeneratorTests.swift
@@ -6,14 +6,24 @@ final class FileGeneratorTests: XCTestCase {
 
     func testInit() {
         do {
-            let generator = FileGenerator(lines: "Hello", "World")
+            let generator = FileGenerator(lines: ["Hello", "World"])
             XCTAssertEqual(generator.indentation, .spaces(4))
             XCTAssertEqual(generator.lines, ["Hello", "World"])
         }
         do {
-            let generator = FileGenerator(indentation: .tab, lines: "Hello")
+            let generator = FileGenerator(indentation: .tab, lines: ["Hello"])
             XCTAssertEqual(generator.indentation, .tab)
             XCTAssertEqual(generator.lines, ["Hello"])
         }
+    }
+
+    func testBuilder() {
+        let generator = FileGenerator(indentation: .spaces(2)) {
+            Line("Hello")
+            Line("World")
+        }
+
+        XCTAssertEqual(generator.indentation, .spaces(2))
+        XCTAssertEqual(generator.lines, ["Hello", "World"])
     }
 }
