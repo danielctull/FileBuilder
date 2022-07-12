acd39e65a82814bcbbce9a271059fd6512ce94f9
Daniel Tull <dt@danieltull.co.uk>
2022-07-12 17:49:29 +0100

    Add indentation

--- Sources/FileGenerator/FileGenerator.swift
+++ Sources/FileGenerator/FileGenerator.swift
@@ -1,3 +1,11 @@
 
-struct FileGenerator {
+public struct FileGenerator {
+
+    public let indentation: Indentation
+
+    public init(
+        indentation: Indentation = .spaces(4)
+    ) {
+        self.indentation = indentation
+    }
 }
--- Tests/FileGeneratorTests/FileGeneratorTests.swift
+++ Tests/FileGeneratorTests/FileGeneratorTests.swift
@@ -3,4 +3,10 @@ import FileGenerator
 import XCTest
 
 final class FileGeneratorTests: XCTestCase {
+
+    func testInit() {
+        XCTAssertEqual(FileGenerator().indentation, .spaces(4))
+        XCTAssertEqual(FileGenerator(indentation: .tab).indentation, .tab)
+        XCTAssertEqual(FileGenerator(indentation: .spaces(2)).indentation, .spaces(2))
+    }
 }
