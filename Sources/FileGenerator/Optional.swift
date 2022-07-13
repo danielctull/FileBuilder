f94f4a42f5dba2aae9105612f0d75598d447622f
Daniel Tull <dt@danieltull.co.uk>
2022-07-13 20:51:53 +0100

    Use map to generate the lines for optional content

--- Sources/FileGenerator/Optional.swift
+++ Sources/FileGenerator/Optional.swift
@@ -3,10 +3,7 @@ extension Optional: Content where Wrapped: Content {
 
     public var body: some Content {
         Builtin { indentation in
-            switch self {
-            case .none: return []
-            case .some(let content): return content.generate(indentation: indentation)
-            }
+            map { $0.generate(indentation: indentation) } ?? []
         }
     }
 }
