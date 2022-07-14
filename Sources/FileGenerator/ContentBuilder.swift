aa4da7328bca623361a2cd9b6d9c1e60467523fa
Daniel Tull <dt@danieltull.co.uk>
2022-07-14 11:58:38 +0100

    Remove ambiguity errors
    These seemed to occur because there were different build* functions which took the same input – although this seemed pretty impossible to track down. By using concrete types to pass through the builder, we can remove any ambiguity.

--- Sources/FileGenerator/ContentBuilder.swift
+++ Sources/FileGenerator/ContentBuilder.swift
@@ -2,31 +2,61 @@
 @resultBuilder
 public enum ContentBuilder {
 
-    public static func buildPartialBlock(first: some Content) -> some Content {
+    // To remove ambiguity errors, all Content is type erased to the following
+    // Component and FinalResult types. This also allows us to more easily see
+    // the flow of the calls through the result builder functions.
+
+    public struct Component {
+        fileprivate let generate: (Indentation) -> [Line]
+    }
+
+    public struct FinalResult: Content {
+        fileprivate let generate: (Indentation) -> [Line]
+        public var body: some Content {
+            Builtin(_generate: generate)
+        }
+    }
+
+    public static func buildExpression(_ expression: some Content) -> Component {
+        Component(generate: expression.generate)
+    }
+
+    public static func buildPartialBlock(first: Component) -> Component {
         first
     }
 
-    public static func buildPartialBlock(accumulated: some Content, next: some Content) -> some Content {
-        AccumulatedContent(first: accumulated, second: next)
+    public static func buildPartialBlock(accumulated: Component, next: Component) -> Component {
+        Component { indentation in
+            let first = accumulated.generate(indentation)
+            let second = next.generate(indentation)
+            return first + second
+        }
     }
 
-    public static func buildArray<C: Content>(_ array: [C]) -> [C] {
-        array
+    public static func buildArray(_ array: [Component]) -> Component {
+        Component { indentation in
+            array.flatMap { $0.generate(indentation) }
+        }
     }
 
-    public static func buildEither<C: Content>(first: C) -> C {
+    public static func buildEither(first: Component) -> Component {
         first
     }
 
-    public static func buildEither<C: Content>(second: C) -> C {
+    public static func buildEither(second: Component) -> Component {
         second
     }
 
-    public static func buildOptional<C: Content>(_ optional: C?) -> C? {
-        optional
+    public static func buildOptional(_ optional: Component?) -> Component {
+        Component { indentation in
+            switch optional {
+            case .none: return []
+            case .some(let component): return component.generate(indentation)
+            }
+        }
     }
 
-    public static func buildFinalResult(_ component: some Content) -> some Content {
-        component
+    public static func buildFinalResult(_ component: Component) -> FinalResult {
+        FinalResult(generate: component.generate)
     }
 }
