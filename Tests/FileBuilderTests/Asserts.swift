66b0a97b7b177da813ebc4e3ee44b841c7108ade
Daniel Tull <dt@danieltull.co.uk>
2022-07-15 08:29:37 +0100

    Use ContentBuilder-based assert function

--- Tests/FileBuilderTests/Asserts.swift
+++ Tests/FileBuilderTests/Asserts.swift
@@ -2,21 +2,6 @@
 import FileBuilder
 import XCTest
 
-public func AssertOutput<C: Content>(
-    _ content: @autoclosure () throws -> C,
-    _ value: @autoclosure () throws -> String,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #filePath,
-    line: UInt = #line
-) {
-    XCTAssertEqual(
-        try content().content,
-        try value(),
-        message(),
-        file: file,
-        line: line)
-}
-
 public func AssertContent<C: Content>(
     @ContentBuilder content: () -> C,
     is output: () -> String,
