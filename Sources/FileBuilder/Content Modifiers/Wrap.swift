91ad2d3a3f53a2855d343620c906b4d0a371d526
Daniel Tull <dt@danieltull.co.uk>
2022-07-26 10:53:34 +0100

    Improve function name

--- Sources/FileBuilder/Content Modifiers/Wrap.swift
+++ Sources/FileBuilder/Content Modifiers/Wrap.swift
@@ -4,18 +4,18 @@
 extension Content {
 
     public func wrap(lineLength: Int) -> some Content {
-        modifier(Wrap(length: lineLength))
+        modifier(Wrap(lineLength: lineLength))
     }
 }
 
 private struct Wrap<C: Content>: LineModifier {
 
-    let length: Int
+    let lineLength: Int
 
     func modifyLines(_ lines: [Line]) -> [Line] {
         lines.flatMap { line in
             line.rawValue
-                .split(maximumLineLength: length)
+                .lines(ofLength: lineLength)
                 .map(String.init)
                 .map(Line.init)
         }
@@ -24,23 +24,23 @@ private struct Wrap<C: Content>: LineModifier {
 
 extension StringProtocol {
 
-    fileprivate func split(maximumLineLength: Int) -> [SubSequence] {
+    fileprivate func lines(ofLength lineLength: Int) -> [SubSequence] {
 
-        guard count > maximumLineLength else {
+        guard count > lineLength else {
             return [self[startIndex..<endIndex]]
         }
 
         let head: SubSequence
         let tail: SubSequence
-        if let space = dropLast(count - maximumLineLength).lastIndex(of: " ") {
+        if let space = dropLast(count - lineLength).lastIndex(of: " ") {
             head = self[..<space]
             tail = self[index(after: space)...]
         } else {
-            let end = index(startIndex, offsetBy: maximumLineLength)
+            let end = index(startIndex, offsetBy: lineLength)
             head = self[..<end]
             tail = self[end...]
         }
 
-        return [head] + tail.split(maximumLineLength: maximumLineLength)
+        return [head] + tail.lines(ofLength: lineLength)
     }
 }
