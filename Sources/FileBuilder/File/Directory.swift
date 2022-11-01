d1824a1d06a993eabc9b6d037ff280eb675a2bdc
Daniel Tull <dt@danieltull.co.uk>
2022-11-01 09:42:28 +0000

    Add Directory

---
+++ Sources/FileBuilder/File/Directory.swift
@@ -0,0 +1,16 @@
+
+import Foundation
+
+public struct Directory<Content: File> {
+    public let name: String
+    private let content: Content
+}
+
+extension Directory: File {
+
+    public func write(in directory: URL) throws {
+        let url = directory.appendingPathComponent(name)
+        try FileManager().createDirectory(at: url, withIntermediateDirectories: false)
+        try content.write(in: url)
+    }
+}
